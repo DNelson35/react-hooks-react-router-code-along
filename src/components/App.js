215ba31bc7941094a4cc88e0474f6132c3571b73
Damien Nelson <dhnisaman@gmail.com>
2023-02-06 11:10:45 -0500

    commit tree f06dc37d98add6eded445e72e11ab66455cfe9e6

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React from "react"
+import {Route, Switch} from "react-router-dom"
+import About from "./About"
+import Login from "./Login"
+import Home from "./Home"
+import NavBar from "./NavBar"
+
+
+export default function App() {
+    return (
+      <div>
+        <NavBar />
+        <Switch>
+            <Route path='/about'>
+              <About />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+        </Switch>
+      </div>
+    );
+  }
